c4e1d826e5f7661c2a2890690f03092af28f864e
shoji360paxtech <109393555+shoji360paxtech@users.noreply.github.com>
2025-06-26 21:33:22 +0900

    Update autofill4formrun.js

--- malnage/test2/autofill4formrun.js
+++ malnage/test2/autofill4formrun.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',function (){
 let formrundiv=document.getElementById('formrun-embed')
 let formrunifm= formrundiv.getElementsByTagName('iframe')[0]
-formrundiv.insertAdjacentHTML('beforebegin','<div style="font-size:13px;width:100%;margin-bottom:10px;">マイページにログインして情報を登録すると、メールアドレス選択で基本情報が自動で入力されます。</div><div id="malaccountdiv" style="width:100%;padding:auto;text-align:center"></div>')
+formrundiv.insertAdjacentHTML('beforebegin','<div style="font-size:13px;width:100%;margin-bottom:10px;">マイページに<a href="https://inden-seminar.com/doc_mypage/" target="_new">ログイン</a>して情報を登録すると、メールアドレス選択で基本情報が自動で入力されます。</div><div id="malaccountdiv" style="width:100%;padding:auto;text-align:center"></div>')
 fetch("https://script.googleusercontent.com/a/macros/malnage.jp/echo?user_content_key=AehSKLjXnRcwGhzwi1Ch0qN4DfF-ku776xxrYAGNDWNx4-ji26-N2K0PrtXscghc9F58vJqbyoG7rPOJpo4Laj9i2kNBumaISpOUQJnodi-wcrFrMhJImnAX8aNaMQ3JiwM8J1MFNLjFxHOQJv8vl8DP1pzqZnIn8655QC2U7b7bROPtNygv0ZSMPqCdFpoFOm3A8VYnj9bfh5k0bxy1uxO808XwuqISJJivj_KXPWKhZ5NmZPDXXH--2qONQwiWT6cvrgUKVrf1TCtle3zjyf_ZG4mBdvAZ_tlOz6M_9ssz7dfYxfUatw8&lib=MoRvMrDrtWwSQealFxIT8HKuGF7ZTdC29")
     .then(response => response.json())
     .then(
