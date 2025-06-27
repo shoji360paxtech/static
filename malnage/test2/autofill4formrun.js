a672ce3efa2f37a2c0ad0e8f2b7fb260852aa212
shoji360paxtech <109393555+shoji360paxtech@users.noreply.github.com>
2025-06-27 18:43:12 +0900

    Update autofill4formrun.js

--- malnage/test2/autofill4formrun.js
+++ malnage/test2/autofill4formrun.js
@@ -3,9 +3,10 @@ localStorage.setItem('maluuid',maluuid);
 let gaswebapp='https://script.google.com/macros/s/AKfycbxd6tVV_0Va1Pag9cu8XZak6l5PeIJT7B61t401vY_ON1-UBXflkxXN8KFCSPWW2xg_zg/exec'
 window.addEventListener('load',function (){
 let formrundiv=document.getElementById('formrun-embed')
+let formid=formrundiv.getAttribute('data-formrun-form')
 let formrunifm= formrundiv.getElementsByTagName('iframe')[0]
 formrundiv.insertAdjacentHTML('beforebegin','<div style="font-size:13px;width:100%;margin-bottom:10px;">マイページに<a href="https://inden-seminar.com/doc_mypage/" target="_new">ログイン</a>して情報を登録すると、メールアドレス選択で基本情報が自動で入力されます。</div><div id="malaccountdiv" style="width:100%;padding:auto;text-align:center"></div>')
-fetch(gaswebapp+'?maluuid='+maluuid)
+fetch(gaswebapp+'?maluuid='+maluuid+'&formid='+formid)
     .then(response => response.json())
     .then(
     res => {
