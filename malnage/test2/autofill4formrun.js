05508237fa1b26f8349a38feefc8123010ca4c53
shoji360paxtech <109393555+shoji360paxtech@users.noreply.github.com>
2025-07-02 10:14:14 +0900

    Update autofill4formrun.js

--- malnage/test2/autofill4formrun.js
+++ malnage/test2/autofill4formrun.js
@@ -10,7 +10,7 @@ function syncFormrun(){
 let formrundiv=document.getElementById('formrun-embed')
 let formid=formrundiv.getAttribute('data-formrun-form')
 let formrunifm= formrundiv.getElementsByTagName('iframe')[0]
-formrundiv.insertAdjacentHTML('beforebegin',`<div style="font-size:100%;width:100%;margin-bottom:10px;">メールアドレスが表示されていれば基本情報が自動で入力されます。「情報更新」で最新データに更新できます。</div>
+formrundiv.insertAdjacentHTML('beforebegin',`<div style="display:none;font-size:100%;width:100%;margin-bottom:10px;">メールアドレスが表示されていれば基本情報が自動で入力されます。「情報更新」で最新データに更新できます。</div>
 <div id="malaccountdiv" style="min-height:80px;display:none;width:100%;padding:auto;text-align:center"></div>`)
 
 fetch(gaswebapp+'?action=getinfo&maluuid='+maluuid+'&formid='+formid)
